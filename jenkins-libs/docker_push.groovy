5c209c5898d0ea286a2fdcf3f71d6b30c6b2f7bc
Gagandeep Singh <gagan13.ca@gmail.com>
2025-09-28 17:15:24 +0530

    Create docker_push.groovy

---
+++ jenkins-libs/docker_push.groovy
@@ -0,0 +1,8 @@
+def call(String cred_id,String image){
+  withCredentials([usernamePassword(
+    credentialsId: "${cred_id}",
+    usernameVariable: "DOCKER_USER",
+    passwordVariable: "DOCKER_PASS")]){
+  sh "echo ${env.DOCKER_PASS} | docker login -u ${env.DOCKER_USER} --password-stdin"
+  sh "docker push ${env.DOCKER_USER}/${image}"
+}
